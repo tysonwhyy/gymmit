361a10aafb1edad274e15784fba79e373214b889
tysonwhyy <128116726+tysonwhyy@users.noreply.github.com>
2025-01-27 14:10:36 -0500

    updated config

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styles from "./login.module.css";
-import { doSignInWithGoogle } from "../auth";
+import { doSignInWithGoogle } from "../auth.ts";
 
 interface Props {
   setUserId: React.Dispatch<React.SetStateAction<string | null>>;
